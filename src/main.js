099f7553140b25e5c7f9ab8e7ecc1ee7cb7a5af9
rijal <jeryzal333@gmail.com>
2022-09-18 03:21:50 +0700

    mount app once

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { onAuthStateChanged } from "firebase/auth";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { auth } from "./firebase/firebaseApp";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+let app;
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
